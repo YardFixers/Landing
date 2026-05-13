a4d0e877bb245e48e66364e1932751b6da1761be
YardFixers <yardfixers00@gmail.com>
2026-05-12 21:30:47 -0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,140 @@
+// FILE NAME: dashboard.js
+
+/* =========================
+   DEV ACCOUNT
+========================= */
+
+const OWNER_USERNAME =
+"YardFixers";
+
+const OWNER_PASSWORD =
+"+zr4bf+=ef#4";
+
+/* =========================
+   FORCE LOGOUT ON REFRESH
+========================= */
+
+window.addEventListener(
+"beforeunload",
+()=>{
+
+sessionStorage.removeItem(
+"ownerLoggedIn"
+);
+
+});
+
+/* =========================
+   LOGIN
+========================= */
+
+const loginScreen =
+document.getElementById(
+"loginScreen"
+);
+
+const dashboardContainer =
+document.getElementById(
+"dashboardContainer"
+);
+
+dashboardContainer.style.display =
+"none";
+
+function loginOwner(){
+
+const username =
+document.getElementById(
+"ownerUser"
+).value;
+
+const password =
+document.getElementById(
+"ownerPass"
+).value;
+
+const error =
+document.getElementById(
+"loginError"
+);
+
+if(
+username === OWNER_USERNAME &&
+password === OWNER_PASSWORD
+){
+
+sessionStorage.setItem(
+"ownerLoggedIn",
+"true"
+);
+
+loginScreen.style.display =
+"none";
+
+dashboardContainer.style.display =
+"block";
+
+loadDashboard();
+
+}else{
+
+error.innerText =
+"Incorrect Login";
+
+}
+
+}
+
+/* =========================
+   LOGOUT
+========================= */
+
+function logoutOwner(){
+
+sessionStorage.removeItem(
+"ownerLoggedIn"
+);
+
+location.reload();
+
+}
+
+/* =========================
+   BACK TO SITE
+========================= */
+
+function backToSite(){
+
+const confirmLeave =
+confirm(
+"Leaving the owner page will log you out. Continue?"
+);
+
+if(confirmLeave){
+
+sessionStorage.removeItem(
+"ownerLoggedIn"
+);
+
+window.location.href =
+"index.html";
+
+}
+
+}
+
+/* =========================
+   LOAD DASHBOARD
+========================= */
+
 async function loadDashboard(){
 
 try{
 
 const response =
-await fetch("/dashboard-data");
+await fetch(
+"/dashboard-data"
+);
 
 const data =
 await response.json();
@@ -49,27 +180,27 @@ requests.innerHTML += `
 <div class="dash-item">
 
 <h3>
-${order.name || "Unknown"}
+${order.Name || "Unknown"}
 </h3>
 
 <p>
 <b>Email:</b>
-${order.email || ""}
+${order.Email || ""}
 </p>
 
 <p>
 <b>Phone:</b>
-${order.phone || ""}
+${order.Phone || ""}
 </p>
 
 <p>
 <b>Area:</b>
-${order.area || ""}
+${order.Area || ""}
 </p>
 
 <p>
 <b>Message:</b>
-${order.message || ""}
+${order.Message || ""}
 </p>
 
 </div>
@@ -97,13 +228,14 @@ visitors.innerHTML += `
 <div class="dash-item">
 
 <p>
-<b>IP:</b>
-${visitor.ip || "Unknown"}
+<b>Screen:</b>
+${visitor.width || 0} x
+${visitor.height || 0}
 </p>
 
 <p>
-<b>Screen:</b>
-${visitor.width || 0}x${visitor.height || 0}
+<b>Visited:</b>
+${visitor.time || ""}
 </p>
 
 </div>
@@ -119,5 +251,3 @@ console.log(error);
 }
 
 }
-
-loadDashboard();
