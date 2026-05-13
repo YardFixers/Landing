af48116abef3366bc3f4cc6fe99958dd99964d96
YardFixers <yardfixers00@gmail.com>
2026-05-12 20:45:14 -0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,113 +1,152 @@
-function login(){
-
-const user =
-document.getElementById(
-"user"
-).value;
-
-const pass =
-document.getElementById(
-"pass"
-).value;
-
-/* DEV ACCOUNT */
-
-if(
-
-user==="YardFixers" &&
-
-pass==="+zr4bf+=ef#4"
-
-){
-
-document.getElementById(
-"dashboard"
-).style.display="block";
-
-loadDashboard();
-
-}else{
-
-alert("Wrong Login");
-
-}
-
-}
-
-/* LOAD ANALYTICS */
-
 async function loadDashboard(){
 
-const response=
-await fetch("/dashboard");
+const response =
+await fetch("/dashboard-data");
 
-const data=
+const data =
 await response.json();
 
-const stats=
+/* COUNTS */
+
 document.getElementById(
-"stats"
+"visitorCount"
+).innerText =
+data.visitors.length;
+
+document.getElementById(
+"requestCount"
+).innerText =
+data.orders.length;
+
+document.getElementById(
+"feedbackCount"
+).innerText =
+data.feedback.length;
+
+document.getElementById(
+"userCount"
+).innerText =
+data.users.length;
+
+/* REQUESTS */
+
+const requests =
+document.getElementById(
+"requestsContainer"
 );
 
-stats.innerHTML=`
+data.orders.reverse().forEach(order=>{
 
-<h2>
-Visitors:
-${data.visitors.length}
-</h2>
+requests.innerHTML += `
 
-<h2>
-Orders:
-${data.orders.length}
-</h2>
+<div class="dash-item">
 
-${data.orders.map(order=>`
-
-<div class="card">
-
-<h2>
+<h3>
 ${order.name}
-</h2>
+</h3>
 
 <p>
-Email:
+<b>Email:</b>
 ${order.email}
 </p>
 
 <p>
-Phone:
+<b>Phone:</b>
 ${order.phone}
 </p>
 
 <p>
-Area:
+<b>Area:</b>
 ${order.area}
 </p>
 
 <p>
-Services:
+<b>Services:</b>
 ${order.services}
 </p>
 
 <p>
-Yard:
-${order.yardSize}
+<b>Price:</b>
+${order.price}
 </p>
 
 <p>
-Status:
-${order.status}
-</p>
-
-<p>
-IP:
-${order.ip}
+<b>Message:</b>
+${order.message}
 </p>
 
 </div>
 
-`).join("")}
+`;
+
+});
+
+/* FEEDBACK */
+
+const feedback =
+document.getElementById(
+"feedbackContainer"
+);
+
+data.feedback.reverse().forEach(item=>{
+
+feedback.innerHTML += `
+
+<div class="dash-item">
+
+<h3>
+${item.name}
+</h3>
+
+<p>
+<b>Email:</b>
+${item.email}
+</p>
+
+<p>
+${item.message}
+</p>
+
+</div>
 
 `;
 
+});
+
+/* VISITORS */
+
+const visitors =
+document.getElementById(
+"visitorContainer"
+);
+
+data.visitors.reverse().forEach(visitor=>{
+
+visitors.innerHTML += `
+
+<div class="dash-item">
+
+<p>
+<b>IP:</b>
+${visitor.ip}
+</p>
+
+<p>
+<b>Screen:</b>
+${visitor.width}x${visitor.height}
+</p>
+
+<p>
+<b>Time On Site:</b>
+${visitor.timeSpent} seconds
+</p>
+
+</div>
+
+`;
+
+});
+
 }
+
+loadDashboard();
